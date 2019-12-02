7ff9084c29fab249689a93dfaae2f059b74f8d9f
MinnaAH <minna.hannula@eng.tamk.fi>
2019-12-02 18:25:53 +0200

    getRepo function

--- app/src/main/java/com/example/gitapi_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/gitapi_kotlin/MainActivity.kt
@@ -2,11 +2,47 @@ package com.example.gitapi_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun getRepo(user: String){
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.github.com/users/"+user+"/repos"
+
+        val jsonArrayRequest = JsonArrayRequest(Request.Method.GET, url, null, Response.Listener<JSONArray> {
+            response ->
+            recyclerView.adapter = RepoAdapter(response)
+        },
+        Response.ErrorListener { error ->
+            Log.d("JSON Error",error.toString()) }
+        )
+        queue.add(jsonArrayRequest)
+    }
+
+    fun userRepos(view: View){
+        val user = username.text.toString()
+
+        if(user.isNullOrEmpty()){
+            Log.d("Username error:", user)
+        }
+        else{
+            getRepo(user)
+        }
     }
 }
