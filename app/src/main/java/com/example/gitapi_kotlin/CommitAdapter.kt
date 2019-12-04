6bf75495abc20e9165799141b5a3285268e263e0
MinnaAH <minna.hannula@eng.tamk.fi>
2019-12-04 14:39:18 +0200

    Load image with Picasso

--- app/src/main/java/com/example/gitapi_kotlin/CommitAdapter.kt
+++ app/src/main/java/com/example/gitapi_kotlin/CommitAdapter.kt
@@ -3,6 +3,7 @@ package com.example.gitapi_kotlin
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.commit_item.view.*
@@ -10,6 +11,7 @@ import com.example.gitapi_kotlin.CommitActivity.Companion.authorName
 import com.example.gitapi_kotlin.CommitActivity.Companion.avatar
 import com.example.gitapi_kotlin.CommitActivity.Companion.date
 import com.example.gitapi_kotlin.CommitActivity.Companion.message
+import com.squareup.picasso.Picasso
 
 class CommitAdapter() : RecyclerView.Adapter<CommitAdapter.ViewHolder>() {
 
@@ -23,18 +25,19 @@ class CommitAdapter() : RecyclerView.Adapter<CommitAdapter.ViewHolder>() {
 
     override fun getItemCount(): Int = date.size
 
-    override fun onBindViewHolder(holder: CommitAdapter.ViewHolder, position: Int) {
-        println("author[position]"+authorName[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.authorView.text = authorName[position]
         holder.dateView.text = date[position]
         holder.timeView.text = date[position]
         holder.messageView.text = message[position]
 
-        //holder.authorView.text = authorObj["name"].toString()
-        /*Glide.with(holder.pictureImageView.context)
-            .load(commit["image"].toString())
-            .into(holder.pictureImageView)
-        holder.authorView.text = commit["commit"].toString()*/
+        if(avatar[position] != ""){
+            Picasso
+                .get()
+                .load(avatar[position])
+                .fit()
+                .into(holder.pictureImageView)
+        }
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
@@ -43,7 +46,7 @@ class CommitAdapter() : RecyclerView.Adapter<CommitAdapter.ViewHolder>() {
         val dateView: TextView = view.date
         val timeView: TextView = view.time
         val messageView: TextView = view.message
-        //var pictureImageView: ImageView = view.avatar
+        var pictureImageView: ImageView = view.avatar
 
     }
 }
